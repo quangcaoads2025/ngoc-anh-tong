9a95667a7eb86daa0613a1d11a8a450603bd28a7
quangcaoads2025 <quangcaoads2025@gmail.com>
2026-07-11 00:05:24 +0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.NA_SUPABASE = Object.freeze({
   url: "https://zjzblybvhvlxqjailfop.supabase.co",
-  publishableKey: "PASTE_SUPABASE_PUBLISHABLE_KEY_HERE",
+  publishableKey: "sb_publishable_zw_r1tBBVV8fjH68rcGiOg_0gisKEJD",
   storageBucket: "site-media"
 });

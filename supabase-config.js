0430e1087b4c0e8dde7e5e3687f33729172a496b
quangcaoads2025 <quangcaoads2025@gmail.com>
2026-07-13 17:17:53 +0700

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-window.NA_SUPABASE = Object.freeze({
-  url: "https://zjzblybvhvlxqjailfop.supabase.co",
-  publishableKey: "sb_publishable_zw_r1tBBVV8fjH68rcGiOg_0gisKEJD",
-  storageBucket: "site-media"
-});
